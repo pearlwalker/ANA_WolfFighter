c33f023b113fe82ff14d58bac3c0683c8763d18d
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-12-12 23:49:42 -0500

    Healthbar + Punch()
    Moved variable declarations to start of JS file, out of function
    
    Once game has been started, an event listener is added for keydown, checks if spacebar has been pressed, checks if the phase is phase1, then calls "punch"

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+let contentBox = document.getElementById("contentbox");
+
+//create ANA image element
+let ana = document.createElement("img");
+ana.src = "media/sprites/ana/ana-2.png";
+ana.id = "ana";
+
+//create wolf image element
+let wolf = document.createElement("img");
+wolf.src = "media/sprites/wolf/A-1.png";
+wolf.id = "wolf";
+
+//create background
+let backdrop = document.createElement("img");
+backdrop.src = "media/sprites/other/background1.png";
+backdrop.id = "background_image";
+
+//create healthbar
+let healthbar = document.createElement("img");
+healthbar.src = "media/sprites/other/healthbar_1-7.png";
+healthbar.id = "healthbar"
+
+let phase = 0; //phase variable
+let health = 1;
 function buttonClick() {
     console.log("button clicked!");
     startGame();
@@ -9,31 +33,49 @@ function startGame() {
     startButton.remove();
 
     //background change to show game start
-    let contentBox = document.getElementById("contentbox");
+
     contentBox.style.backgroundColor = "white";
 
-    //create ANA image element
-    let ana = document.createElement("img");
-    ana.src = "media/sprites/ana/ana-2.png";
-    ana.id = "ana";
-
-    //create wolf image element
-    let wolf = document.createElement("img");
-    wolf.src = "media/sprites/wolf/A-1.png";
-    wolf.id = "wolf";
-
-    //create background
-    let backdrop = document.createElement("img");
-    backdrop.src = "media/sprites/other/background1.png";
-    backdrop.id = "background_image";
-
-    //create healthbar
-    let healthbar = document.createElement("img");
-    healthbar.src = "media/sprites/other/healthbar_7-7.png";
-    healthbar.id = "healthbar"
     //add to contentBox
     contentBox.appendChild(ana);
     contentBox.appendChild(wolf);
     contentBox.appendChild(backdrop);
     contentBox.appendChild(healthbar);
+
+    //start phase1 of game
+    partOne();
+}
+
+function partOne() {
+    phase = 1;
+    document.addEventListener("keydown", function (event) {
+        if (event.keyCode == 32) {
+            if (phase === 1) {
+                punch();
+            }
+        }
+    })
+}
+
+function punch() {
+    console.log("punch!");
+    if (health === 1) {
+        healthbar.src = "media/sprites/other/healthbar_2-7.png";
+        health = 2;
+    } else if (health === 2) {
+        healthbar.src = "media/sprites/other/healthbar_3-7.png";
+        health = 3;
+    } else if (health === 3) {
+        healthbar.src = "media/sprites/other/healthbar_4-7.png";
+        health = 4;
+    } else if (health === 4) {
+        healthbar.src = "media/sprites/other/healthbar_5-7.png";
+        health = 5;
+    } else if (health === 5) {
+        healthbar.src = "media/sprites/other/healthbar_6-7.png";
+        health = 6;
+    } else if (health === 6) {
+        healthbar.src = "media/sprites/other/healthbar_7-7.png";
+        health = 7;
+    }
 }
