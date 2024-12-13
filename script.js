bd066e353397026a71b3ef02c84c493b515828aa
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-12-13 01:05:45 -0500

    SFX implementation
    Implemented punch sound effects. It's not the same sound each time, and that's called ATTENTION TO DETAIL!
    
    Oh also to hide the healthbar between phases, I changed its z-index since it was being screwy

--- script.js
+++ script.js
@@ -22,6 +22,12 @@ healthbar.id = "healthbar"
 
 let phase = 0; //phase variable
 let health = 0;
+
+let sfx_punch1 = new Audio("media/audio/sail1.mp3");
+let sfx_punch2 = new Audio("media/audio/sail2.mp3");
+let sfx_punch3 = new Audio("media/audio/sail3.mp3");
+let sfx_punch4 = new Audio("media/audio/sail3-2.mp3");
+
 function buttonClick() {
     console.log("button clicked!");
     startGame();
@@ -70,45 +76,66 @@ function punch() {
     ana.src = "media/sprites/ana/punch-ana-2.png";
     wolf.src = wolf.src = "media/sprites/wolf/A-2.png";
     if (health === 0) {
+        pickSFX();
         healthbar.src = "media/sprites/other/healthbar_1-7.png";
         backdrop.src = "media/sprites/other/background1_1-7.png";
         health = 1;
     } else if (health === 1) {
+        pickSFX();
         healthbar.src = "media/sprites/other/healthbar_2-7.png";
         backdrop.src = "media/sprites/other/background1_2-7.png";
         health = 2;
     } else if (health === 2) {
+        pickSFX();
         healthbar.src = "media/sprites/other/healthbar_3-7.png";
         backdrop.src = "media/sprites/other/background1_3-7.png";
         health = 3;
     } else if (health === 3) {
+        pickSFX();
         healthbar.src = "media/sprites/other/healthbar_4-7.png";
         backdrop.src = "media/sprites/other/background1_4-7.png";
         health = 4;
     } else if (health === 4) {
+        pickSFX();
         healthbar.src = "media/sprites/other/healthbar_5-7.png";
         backdrop.src = "media/sprites/other/background1_5-7.png";
         health = 5;
     } else if (health === 5) {
+        pickSFX();
         healthbar.src = "media/sprites/other/healthbar_6-7.png";
         backdrop.src = "media/sprites/other/background1_6-7.png";
         health = 6;
     } else if (health === 6) {
-        healthbar.src = "media/sprites/other/healthbar_7-7.png";
+        pickSFX();
         backdrop.src = "media/sprites/other/background1_7-7.png";
         wolf.src = "";
         ana.src = "";
+        healthbar.style.zIndex = -2;
         health = 7;
     } else if (health === 7) {
         partTwo();
     }
 }
-
+function pickSFX() {
+    if (phase === 1) {
+        let random = Math.floor((Math.random() * 2));
+        if (random === 0) {
+            sfx_punch1.play();
+        } else if (random === 1) {
+            sfx_punch2.play();
+        } else if (random === 2) {
+            sfx_punch3.play();
+        } else {
+            sfx_punch4.play();
+        }
+    }
+}
 function unpunch() {
     console.log("unpunch!");
-    if (health === 7){
+    if (health === 7) {
         wolf.src = "";
         ana.src = "";
+        healthbar.style.zIndex = -2;
     } else {
         wolf.src = wolf.src = "media/sprites/wolf/A-1.png";
         ana.src = "media/sprites/ana/ana-2.png";
@@ -121,4 +148,5 @@ function partTwo() {
     health = 0;
     wolf.src = wolf.src = "media/sprites/wolf/B-1.png";
     ana.src = "media/sprites/ana/ana-2.png";
+    healthbar.style.zIndex = 1;
 } 
