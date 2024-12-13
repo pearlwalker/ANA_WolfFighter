a0c15a2bc06e3f0a245e0171334fd2ea621774ee
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2024-12-13 00:32:10 -0500

    Background animation + Hide on 7th punch
    Each time you punch, the background changes
    
    after you get 7 punches, and before you start part2, the sprites hide

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ wolf.id = "wolf";
 
 //create background
 let backdrop = document.createElement("img");
-backdrop.src = "media/sprites/other/background1.png";
+backdrop.src = "media/sprites/other/background1_0-7.png";
 backdrop.id = "background_image";
 
 //create healthbar
 let healthbar = document.createElement("img");
-healthbar.src = "media/sprites/other/healthbar_1-7.png";
+healthbar.src = "media/sprites/other/healthbar_0-7.png";
 healthbar.id = "healthbar"
 
 let phase = 0; //phase variable
-let health = 1;
+let health = 0;
 function buttonClick() {
     console.log("button clicked!");
     startGame();
@@ -48,6 +48,7 @@ function startGame() {
 
 function partOne() {
     phase = 1;
+    backdrop.src = "media/sprites/other/background1_0-7.png";
     document.addEventListener("keydown", function (event) {
         if (event.keyCode == 32) {
             if (phase === 1) {
@@ -68,23 +69,35 @@ function punch() {
     console.log("punch!");
     ana.src = "media/sprites/ana/punch-ana-2.png";
     wolf.src = wolf.src = "media/sprites/wolf/A-2.png";
-    if (health === 1) {
+    if (health === 0) {
+        healthbar.src = "media/sprites/other/healthbar_1-7.png";
+        backdrop.src = "media/sprites/other/background1_1-7.png";
+        health = 1;
+    } else if (health === 1) {
         healthbar.src = "media/sprites/other/healthbar_2-7.png";
+        backdrop.src = "media/sprites/other/background1_2-7.png";
         health = 2;
     } else if (health === 2) {
         healthbar.src = "media/sprites/other/healthbar_3-7.png";
+        backdrop.src = "media/sprites/other/background1_3-7.png";
         health = 3;
     } else if (health === 3) {
         healthbar.src = "media/sprites/other/healthbar_4-7.png";
+        backdrop.src = "media/sprites/other/background1_4-7.png";
         health = 4;
     } else if (health === 4) {
         healthbar.src = "media/sprites/other/healthbar_5-7.png";
+        backdrop.src = "media/sprites/other/background1_5-7.png";
         health = 5;
     } else if (health === 5) {
         healthbar.src = "media/sprites/other/healthbar_6-7.png";
+        backdrop.src = "media/sprites/other/background1_6-7.png";
         health = 6;
     } else if (health === 6) {
         healthbar.src = "media/sprites/other/healthbar_7-7.png";
+        backdrop.src = "media/sprites/other/background1_7-7.png";
+        wolf.src = "";
+        ana.src = "";
         health = 7;
     } else if (health === 7) {
         partTwo();
@@ -93,8 +106,14 @@ function punch() {
 
 function unpunch() {
     console.log("unpunch!");
-    wolf.src = wolf.src = "media/sprites/wolf/A-1.png";
-    ana.src = "media/sprites/ana/ana-2.png";
+    if (health === 7){
+        wolf.src = "";
+        ana.src = "";
+    } else {
+        wolf.src = wolf.src = "media/sprites/wolf/A-1.png";
+        ana.src = "media/sprites/ana/ana-2.png";
+    }
+
 
 }
 function partTwo() {
